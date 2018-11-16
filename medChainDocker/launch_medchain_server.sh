f7ae1f94ec49afdeace1c6cb39cd914a04523743
Marin Thiercelin <marin.thiercelin@epfl.ch>
2018-11-16 19:06:08 +0100

    helper scripts for launch

---
+++ medChainDocker/launch_medchain_server.sh
@@ -0,0 +1,12 @@
+rm -rf $GOPATH/src/github.com/talhaparacha/
+cp -rf $GOPATH/src/github.com/DPPH/MedChain/medChainDocker/src/github.com/talhaparacha/ $GOPATH/src/github.com/talhaparacha/
+cp -rf $GOPATH/src/github.com/DPPH/cothority/ $GOPATH/src/github.com/dedis/cothority/
+cp $GOPATH/src/github.com/DPPH/MedChain/medChainDocker/Dockerfile $GOPATH/Dockerfile
+cp $GOPATH/src/github.com/DPPH/MedChain/medChainDocker/.dockerignore $GOPATH/.dockerignore
+cd $GOPATH
+docker build -t medchainserver .
+docker run -d --name=medchainserver1 --rm -p 8989:8989 medchainserver
+sleep 5s
+echo "Starting the server"
+curl http://localhost:8989/start
+docker container attach medchainserver1
