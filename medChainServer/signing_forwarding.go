0babfd0d0eb1e6d11fa195ecfe182229bae45222
Zahra Farsijani <farsijan@icsil1noteb192.local>
2019-11-25 11:34:34 +0100

    Initial commit

--- medChainServer/signing_forwarding.go
+++
@@ -1,8 +0,0 @@
-package main
-
-import "net/http"
-
-// forwards the request to the signing service as a proxy
-func forwardToSigning(w http.ResponseWriter, r *http.Request) {
-	signingProxy.ServeHTTP(w, r)
-}
--- signingService/data/fresh_db.sh
+++
@@ -1,2 +0,0 @@
-rm actions.db
-sqlite3 actions.db < tables.sql
