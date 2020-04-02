8dc114e88d7858ebbcd676322e76c7945c9a9173
jean <jean.chambras@epfl.ch>
2020-04-02 13:12:34 +0200

    Start Admin API
    - Create Admin Darc
    - Create Deffered transactions to add admins
    - Sign Deffered transactions
    - Execute Deffered transactions

---
+++ admin/admin_api.go
@@ -0,0 +1,324 @@
+package admin
+
+import (
+	"encoding/binary"
+	"errors"
+	"strings"
+
+	"go.dedis.ch/cothority/v3/byzcoin"
+	"go.dedis.ch/cothority/v3/byzcoin/bcadmin/lib"
+	"go.dedis.ch/cothority/v3/darc"
+	"go.dedis.ch/cothority/v3/darc/expression"
+	"go.dedis.ch/protobuf"
+	"golang.org/x/xerrors"
+)
+
+type Client struct {
+	bcl           *byzcoin.Client
+	adminkeys     darc.Signer
+	genDarc       darc.Darc
+	signerCounter uint64
+}
+
+func NewClient(bcl *byzcoin.Client) (*Client, error) {
+	if bcl == nil {
+		return nil, errors.New("A Byzcoin Client is required")
+	}
+	client := &Client{
+		bcl:           bcl,
+		adminkeys:     darc.NewSignerEd25519(nil, nil),
+		signerCounter: 1,
+	}
+	if genDarc, err := bcl.GetGenDarc(); err == nil {
+		client.genDarc = *genDarc
+		return client, nil
+	} else {
+		return nil, xerrors.Errorf("getting genesis darc from chain: %w", err)
+	}
+}
+
+func NewClientWithAuth(bcl *byzcoin.Client, keys *darc.Signer) (*Client, error) {
+	if keys == nil {
+		return nil, errors.New("Keys are required")
+	}
+	client, err := NewClient(bcl)
+	client.adminkeys = *keys
+	return client, err
+}
+
+func (cl *Client) AuthKey() darc.Signer {
+	return cl.adminkeys
+}
+
+func (cl *Client) SpawnNewAdminDarc() (*darc.Darc, error) {
+	adminDarc, err := cl.createAdminDarc()
+	if err != nil {
+		return nil, err
+	}
+	adminDarcProto, err := adminDarc.ToProto()
+	if err != nil {
+		return nil, err
+	}
+	ctx, err := cl.bcl.CreateTransaction(byzcoin.Instruction{
+		InstanceID: byzcoin.NewInstanceID(cl.genDarc.GetBaseID()),
+		Spawn: &byzcoin.Spawn{
+			ContractID: byzcoin.ContractDarcID,
+			Args: byzcoin.Arguments{
+				{
+					Name:  "darc",
+					Value: adminDarcProto,
+				},
+			},
+		},
+		// SignerIdentities: []darc.Identity{superAdmin.Identity()},
+		SignerCounter: []uint64{cl.signerCounter},
+	})
+	err = ctx.FillSignersAndSignWith(cl.adminkeys)
+	if err != nil {
+		return nil, xerrors.Errorf("Signing: %w", err)
+	}
+	_, err = cl.bcl.AddTransaction(ctx)
+	if err != nil {
+		return nil, xerrors.Errorf("Adding transaction to the ledger: %w", err)
+	}
+	cl.incrementSignerCounter()
+	return adminDarc, err
+
+}
+
+func (cl *Client) createAdminDarc() (*darc.Darc, error) {
+	adminDarcDescription := "Admin darc guards medchain project darcs"
+	rules := darc.InitRules([]darc.Identity{cl.adminkeys.Identity()}, []darc.Identity{cl.adminkeys.Identity()})
+	adminDarc := darc.NewDarc(rules, []byte(adminDarcDescription))
+	adminDarcActions := "invoke:darc.evolve,spawn:value,spawn:deferred,invoke:deferred.addProof,invoke:deferred.execProposedTx"
+	adminDarcExpr := expression.InitAndExpr(cl.adminkeys.Identity().String())
+	err := AddRuleToDarc(adminDarc, adminDarcActions, adminDarcExpr)
+	return adminDarc, err
+}
+
+func (cl *Client) AddAdminToAdminDarc(adid darc.ID, newAdmin darc.Identity) (byzcoin.InstanceID, error) {
+	adminDarc, err := lib.GetDarcByID(cl.bcl, adid)
+	if err != nil {
+		return *new(byzcoin.InstanceID), xerrors.Errorf("Getting the admin darc from chain: %w", err)
+	}
+	exp := adminDarc.Rules.GetEvolutionExpr()
+	slc := strings.Split(string(exp), "&")
+	for i := range slc {
+		slc[i] = strings.TrimSpace(slc[i])
+	}
+	slc = append(slc, newAdmin.String())
+	evolvedAdminDarc := adminDarc.Copy()
+	newAdminAndExpr := expression.InitAndExpr(slc...)
+	newAdminOrExpr := expression.InitOrExpr(slc...)
+	err = evolvedAdminDarc.Rules.UpdateEvolution(newAdminAndExpr)
+	if err != nil {
+		return *new(byzcoin.InstanceID), xerrors.Errorf("Updating the _evolve expression in admin darc: %w", err)
+	}
+	err = evolvedAdminDarc.Rules.UpdateSign(newAdminOrExpr)
+	if err != nil {
+		return *new(byzcoin.InstanceID), xerrors.Errorf("Updating the _sign expression in admin darc: %w", err)
+	}
+	err = evolvedAdminDarc.Rules.UpdateRule("invoke:deferred.addProof", newAdminOrExpr)
+	if err != nil {
+		return *new(byzcoin.InstanceID), xerrors.Errorf("Updating the invoke:deferred.addProof expression in admin darc: %w", err)
+	}
+	err = evolvedAdminDarc.Rules.UpdateRule("invoke:deferred.execProposedTx", newAdminOrExpr)
+	if err != nil {
+		return *new(byzcoin.InstanceID), xerrors.Errorf("Updating the invoke:deferred.execProposedTx expression in admin darc: %w", err)
+	}
+	err = evolvedAdminDarc.Rules.UpdateRule("invoke:darc.evolve", newAdminAndExpr)
+	if err != nil {
+		return *new(byzcoin.InstanceID), xerrors.Errorf("Updating the invoke:darc.evolve expression in admin darc: %w", err)
+	}
+	err = evolvedAdminDarc.EvolveFrom(adminDarc)
+	if err != nil {
+		return *new(byzcoin.InstanceID), xerrors.Errorf("Evolving the admin darc: %w", err)
+	}
+	_, evolvedAdminDarc2Buf, err := evolvedAdminDarc.MakeEvolveRequest(cl.AuthKey())
+	if err != nil {
+		return *new(byzcoin.InstanceID), xerrors.Errorf("Creating the evolution request: %w", err)
+	}
+
+	proposedTransaction, err := cl.bcl.CreateTransaction(byzcoin.Instruction{
+		InstanceID: byzcoin.NewInstanceID(adminDarc.GetBaseID()),
+		Invoke: &byzcoin.Invoke{
+			ContractID: byzcoin.ContractDarcID,
+			Command:    "evolve",
+			Args: []byzcoin.Argument{{
+				Name:  "darc",
+				Value: evolvedAdminDarc2Buf,
+			}},
+		},
+	})
+	if err != nil {
+		return *new(byzcoin.InstanceID), xerrors.Errorf("Creating the transaction: %w", err)
+	}
+
+	expireBlockIndexInt := uint64(6000)
+	expireBlockIndexBuf := make([]byte, 8)
+	binary.LittleEndian.PutUint64(expireBlockIndexBuf, expireBlockIndexInt)
+	proposedTransactionBuf, err := protobuf.Encode(&proposedTransaction)
+	if err != nil {
+		return *new(byzcoin.InstanceID), xerrors.Errorf("Encoding the proposed transaction: %w", err)
+	}
+	ctx, err := cl.bcl.CreateTransaction(byzcoin.Instruction{
+		InstanceID: byzcoin.NewInstanceID(adid),
+		Spawn: &byzcoin.Spawn{
+			ContractID: byzcoin.ContractDeferredID,
+			Args: []byzcoin.Argument{
+				{
+					Name:  "proposedTransaction",
+					Value: proposedTransactionBuf,
+				},
+				{
+					Name:  "expireBlockIndex",
+					Value: expireBlockIndexBuf,
+				},
+			},
+		},
+		SignerCounter: []uint64{cl.signerCounter},
+	})
+	if err != nil {
+		return *new(byzcoin.InstanceID), xerrors.Errorf("Creating the deffered transaction: %w", err)
+	}
+	err = ctx.FillSignersAndSignWith(cl.adminkeys)
+	if err != nil {
+		return *new(byzcoin.InstanceID), xerrors.Errorf("Signing: %w", err)
+	}
+	_, err = cl.bcl.AddTransactionAndWait(ctx, 10)
+	if err != nil {
+		return *new(byzcoin.InstanceID), xerrors.Errorf("Spawning the deffered transaction: %w", err)
+	}
+	cl.incrementSignerCounter() // Need to check for errors before incrementing the signer counter
+	return ctx.Instructions[0].DeriveID(""), err
+}
+
+func (cl *Client) AddSignatureToDefferedTx(myID byzcoin.InstanceID) error {
+	result, err := cl.bcl.GetDeferredData(myID)
+	if err != nil {
+		return xerrors.Errorf("Getting the deffered instance from chain: %w", err)
+	}
+	rootHash := result.InstructionHashes
+	identity := cl.AuthKey().Identity()
+	identityBuf, err := protobuf.Encode(&identity)
+	if err != nil {
+		return xerrors.Errorf("Encoding the identity of signer: %w", err)
+	}
+	signature, err := cl.AuthKey().Sign(rootHash[0]) // == index
+	if err != nil {
+		return xerrors.Errorf("Signing the deffered transaction: %w", err)
+	}
+	// TODO: Implement multi instructions transactions.
+	index := uint32(0) // The index of the instruction to sign in the transaction
+	indexBuf := make([]byte, 4)
+	binary.LittleEndian.PutUint32(indexBuf, uint32(index))
+
+	ctx, err := cl.bcl.CreateTransaction(byzcoin.Instruction{
+		InstanceID: myID,
+		Invoke: &byzcoin.Invoke{
+			ContractID: byzcoin.ContractDeferredID,
+			Command:    "addProof",
+			Args: []byzcoin.Argument{
+				{
+					Name:  "identity",
+					Value: identityBuf,
+				},
+				{
+					Name:  "signature",
+					Value: signature,
+				},
+				{
+					Name:  "index",
+					Value: indexBuf,
+				},
+			},
+		},
+		SignerCounter: []uint64{cl.signerCounter},
+	})
+	if err != nil {
+		return xerrors.Errorf("Creating the transaction: %w", err)
+	}
+	err = ctx.FillSignersAndSignWith(cl.AuthKey())
+	if err != nil {
+		return xerrors.Errorf("Signing: %w", err)
+	}
+	_, err = cl.bcl.AddTransactionAndWait(ctx, 10)
+	if err != nil {
+		return xerrors.Errorf("Adding transaction to the ledger: %w", err)
+	}
+	cl.incrementSignerCounter()
+	return nil
+}
+
+func (cl *Client) ExecDefferedTx(myID byzcoin.InstanceID) error {
+	ctx, err := cl.bcl.CreateTransaction(byzcoin.Instruction{
+		InstanceID: myID,
+		Invoke: &byzcoin.Invoke{
+			ContractID: byzcoin.ContractDeferredID,
+			Command:    "execProposedTx",
+		},
+		SignerCounter: []uint64{cl.signerCounter},
+	})
+	if err != nil {
+		return xerrors.Errorf("Creating the transaction: %w", err)
+	}
+	err = ctx.FillSignersAndSignWith(cl.AuthKey())
+	if err != nil {
+		return xerrors.Errorf("Signing: %w", err)
+	}
+	_, err = cl.bcl.AddTransactionAndWait(ctx, 10)
+	if err != nil {
+		return xerrors.Errorf("Adding transaction to the ledger: %w", err)
+	}
+	cl.incrementSignerCounter()
+	return nil
+}
+
+func (cl *Client) getLastSignerCounter() uint64 {
+	signerCtrs, _ := cl.bcl.GetSignerCounters(cl.AuthKey().Identity().String())
+	return signerCtrs.Counters[0]
+}
+func (cl *Client) incrementSignerCounter() {
+	cl.signerCounter++
+}
+func (cl *Client) updateSignerCounter(sc uint64) {
+	cl.signerCounter = sc
+}
+
+func (cl *Client) spawnDeferredInstance() {
+
+}
+
+func (cl *Client) RemoveAdminFromAdminDarc() {
+
+}
+
+func (cl *Client) ModifyAdminKeysFromAdminDarc() {
+
+}
+
+func (cl *Client) CreateNewProject() {
+}
+
+func (cl *Client) AddQuerierToProject() {
+
+}
+
+func (cl *Client) RemoveQuerierFromProject() {
+
+}
+
+// TODO Create util package to reuse methods
+
+func AddRuleToDarc(userDarc *darc.Darc, action string, expr expression.Expr) error {
+	actions := strings.Split(action, ",")
+
+	for i := 0; i < len(actions); i++ {
+		dAction := darc.Action(actions[i])
+		err := userDarc.Rules.AddRule(dAction, expr)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
---
+++ admin/admin_api_test.go
@@ -0,0 +1,132 @@
+package admin
+
+import (
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
+	"go.dedis.ch/cothority/v3"
+	"go.dedis.ch/cothority/v3/byzcoin"
+	"go.dedis.ch/cothority/v3/byzcoin/bcadmin/lib"
+	"go.dedis.ch/cothority/v3/darc"
+	"go.dedis.ch/onet/v3"
+	"go.dedis.ch/onet/v3/log"
+)
+
+func TestAddAdminsToDarc(t *testing.T) {
+	// ------------------------------------------------------------------------
+	// 0. Set up
+	// ------------------------------------------------------------------------
+	log.Lvl1("[INFO] Create admin darc")
+	local := onet.NewTCPTest(cothority.Suite)
+	defer local.CloseAll()
+
+	superAdmin := darc.NewSignerEd25519(nil, nil)
+	_, roster, _ := local.GenTree(5, true)
+
+	genesisMsg, err := byzcoin.DefaultGenesisMsg(byzcoin.CurrentVersion, roster,
+		[]string{"spawn:value", "spawn:deferred", "invoke:deferred.addProof",
+			"invoke:deferred.execProposedTx"}, superAdmin.Identity())
+	require.NoError(t, err)
+	gDarc := &genesisMsg.GenesisDarc
+
+	genesisMsg.BlockInterval = time.Second / 5
+	bcl, _, err := byzcoin.NewLedger(genesisMsg, false)
+	require.NoError(t, err)
+	log.Lvl1("[INFO] Create admin client")
+	admcl, err := NewClientWithAuth(bcl, &superAdmin)
+	require.NoError(t, err)
+	require.Equal(t, superAdmin, admcl.AuthKey())
+
+	// ------------------------------------------------------------------------
+	// 1. Spawn admin darc
+	// ------------------------------------------------------------------------
+
+	log.Lvl1("[INFO] Spawn admin darc")
+	adminDarc, err := admcl.SpawnNewAdminDarc()
+	require.NoError(t, err)
+	admcl.bcl.WaitPropagation(1)
+	_, err = lib.GetDarcByID(admcl.bcl, gDarc.GetID()) // Verify that the darc is really in the global state
+	require.NoError(t, err)
+
+	// ------------------------------------------------------------------------
+	// 2. Add a new admin to the admin darc
+	// ------------------------------------------------------------------------
+
+	log.Lvl1("[INFO] Create new admin client (admcl2)")
+	admcl2, err := NewClient(bcl)
+	require.NoError(t, err)
+	log.Lvl1("[INFO] Create deffered tx to add admcl2 identity")
+	id, err := admcl.AddAdminToAdminDarc(adminDarc.GetBaseID(), admcl2.AuthKey().Identity())
+	require.NoError(t, err)
+	admcl.bcl.WaitPropagation(1)
+	_, err = admcl2.bcl.GetDeferredData(id)
+	require.NoError(t, err)
+	log.Lvl1("[INFO] Admin 1 sign the transaction")
+	admcl.AddSignatureToDefferedTx(id)
+	log.Lvl1("[INFO] Admin 1 try to exec the transaction")
+	err = admcl.ExecDefferedTx(id)
+	require.NoError(t, err)
+	log.Lvl1("[INFO] Tx successfully executed, admin 3 has been added to the admin darc")
+	_, err = lib.GetDarcByID(admcl.bcl, adminDarc.GetID()) // Verify that the darc is really in the global state
+	require.NoError(t, err)
+
+	// ------------------------------------------------------------------------
+	// 2. Add a new admin to the admin darc
+	// ------------------------------------------------------------------------
+
+	log.Lvl1("[INFO] Create new admin client (admcl3)")
+	admcl3, err := NewClient(bcl)
+	require.NoError(t, err)
+	log.Lvl1("[INFO] Create deffered tx to add admcl3 identity")
+	id, err = admcl.AddAdminToAdminDarc(adminDarc.GetBaseID(), admcl3.AuthKey().Identity())
+	require.NoError(t, err)
+	admcl.bcl.WaitPropagation(1)
+	_, err = admcl2.bcl.GetDeferredData(id)
+	require.NoError(t, err)
+	log.Lvl1("[INFO] Admin 1 sign the transaction")
+	err = admcl.AddSignatureToDefferedTx(id)
+	require.NoError(t, err)
+	log.Lvl1("[INFO] Admin 1 try to exec the transaction but threshold of signature not reached")
+	err = admcl.ExecDefferedTx(id)
+	require.Error(t, err)
+	log.Lvl1("[INFO] Admin 2 sign the transaction")
+	err = admcl2.AddSignatureToDefferedTx(id)
+	require.NoError(t, err)
+	log.Lvl1("[INFO] Admin 2 try to exec the transaction")
+	err = admcl.ExecDefferedTx(id)
+	require.NoError(t, err)
+	log.Lvl1("[INFO] Admin 2 try to exec the transaction")
+	_, err = lib.GetDarcByID(admcl.bcl, adminDarc.GetID())
+	require.NoError(t, err)
+	log.Lvl1("[INFO] Tx successfully executed, admin 3 has been added to the admin darc")
+
+	// ------------------------------------------------------------------------
+	// 2. Add a new admin to the admin darc
+	// ------------------------------------------------------------------------
+
+	log.Lvl1("[INFO] Create new admin client (admcl4)")
+	admcl4, err := NewClient(bcl)
+	require.NoError(t, err)
+	log.Lvl1("[INFO] Create deffered tx to add admcl4 identity")
+	id, err = admcl.AddAdminToAdminDarc(adminDarc.GetBaseID(), admcl4.AuthKey().Identity())
+	require.NoError(t, err)
+	admcl.bcl.WaitPropagation(1)
+	_, err = admcl2.bcl.GetDeferredData(id)
+	require.NoError(t, err)
+	log.Lvl1("[INFO] Admin 1 sign the transaction")
+	err = admcl.AddSignatureToDefferedTx(id)
+	require.NoError(t, err)
+	log.Lvl1("[INFO] Admin 3 sign the transaction")
+	err = admcl3.AddSignatureToDefferedTx(id)
+	require.NoError(t, err)
+	log.Lvl1("[INFO] Admin 2 sign the transaction")
+	err = admcl2.AddSignatureToDefferedTx(id)
+	require.NoError(t, err)
+	log.Lvl1("[INFO] Admin 3 try to execute the transaction")
+	err = admcl3.ExecDefferedTx(id)
+	require.NoError(t, err)
+	log.Lvl1("[INFO] Tx successfully executed, admin 4 has been added to the admin darc")
+	_, err = lib.GetDarcByID(admcl.bcl, adminDarc.GetID())
+	require.NoError(t, err)
+}
