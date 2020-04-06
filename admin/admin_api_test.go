61f73f7cbdf727ed81af051fe26fc21d7c3803d6
jean <jean.chambras@epfl.ch>
2020-04-06 12:36:34 +0200

    Add modify admins keys functionality

--- admin/admin_api_test.go
+++ admin/admin_api_test.go
@@ -242,3 +242,89 @@ func TestRemovingAdminsToDarc(t *testing.T) {
 	_, err = lib.GetDarcByID(admcl.bcl, adminDarc.GetID())
 	require.NoError(t, err)
 }
+
+func TestUpdateAdminKeys(t *testing.T) {
+	// ------------------------------------------------------------------------
+	// 0. Set up
+	// ------------------------------------------------------------------------
+	log.Lvl1("[INFO] Create admin darc")
+	local := onet.NewTCPTest(cothority.Suite)
+	defer local.CloseAll()
+
+	superAdmin := darc.NewSignerEd25519(nil, nil)
+	_, roster, _ := local.GenTree(5, true)
+
+	genesisMsg, err := byzcoin.DefaultGenesisMsg(byzcoin.CurrentVersion, roster,
+		[]string{"spawn:value", "spawn:deferred", "invoke:deferred.addProof",
+			"invoke:deferred.execProposedTx"}, superAdmin.Identity())
+	require.NoError(t, err)
+	gDarc := &genesisMsg.GenesisDarc
+
+	genesisMsg.BlockInterval = time.Second / 5
+	bcl, _, err := byzcoin.NewLedger(genesisMsg, false)
+	require.NoError(t, err)
+	log.Lvl1("[INFO] Create admin client")
+	admcl, err := NewClientWithAuth(bcl, &superAdmin)
+	require.NoError(t, err)
+	require.Equal(t, superAdmin, admcl.AuthKey())
+
+	// ------------------------------------------------------------------------
+	// 1. Spawn admin darc
+	// ------------------------------------------------------------------------
+
+	log.Lvl1("[INFO] Spawn admin darc")
+	adminDarc, err := admcl.SpawnNewAdminDarc()
+	require.NoError(t, err)
+	admcl.bcl.WaitPropagation(1)
+	_, err = lib.GetDarcByID(admcl.bcl, gDarc.GetID()) // Verify that the darc is really in the global state
+	require.NoError(t, err)
+
+	// ------------------------------------------------------------------------
+	// 2. Add a new admin to the admin darc
+	// ------------------------------------------------------------------------
+
+	log.Lvl1("[INFO] Create new admin client (admcl2)")
+	admcl2, err := NewClient(bcl)
+	require.NoError(t, err)
+	log.Lvl1("[INFO] Create deffered tx to add admcl2 identity")
+	id, err := admcl.AddAdminToAdminDarc(adminDarc.GetBaseID(), admcl2.AuthKey().Identity())
+	require.NoError(t, err)
+	admcl.bcl.WaitPropagation(1)
+	_, err = admcl2.bcl.GetDeferredData(id)
+	require.NoError(t, err)
+	log.Lvl1("[INFO] Admin 1 sign the transaction")
+	admcl.AddSignatureToDefferedTx(id)
+	log.Lvl1("[INFO] Admin 1 try to exec the transaction")
+	err = admcl.ExecDefferedTx(id)
+	require.NoError(t, err)
+	log.Lvl1("[INFO] Tx successfully executed, admin 3 has been added to the admin darc")
+	_, err = lib.GetDarcByID(admcl.bcl, adminDarc.GetID()) // Verify that the darc is really in the global state
+	require.NoError(t, err)
+
+	// ------------------------------------------------------------------------
+	// 2. Add a new admin to the admin darc
+	// ------------------------------------------------------------------------
+
+	log.Lvl1("[INFO] Create new admin client (admcl3)")
+	newAdmcl2, err := NewClient(bcl)
+	require.NoError(t, err)
+	log.Lvl1("[INFO] Create deffered tx to add admcl3 identity")
+	id, err = admcl.ModifyAdminKeysFromAdminDarc(adminDarc.GetBaseID(), admcl2.AuthKey().Identity(), newAdmcl2.AuthKey().Identity())
+	require.NoError(t, err)
+	admcl.bcl.WaitPropagation(1)
+	_, err = admcl2.bcl.GetDeferredData(id)
+	require.NoError(t, err)
+	log.Lvl1("[INFO] Admin 1 sign the transaction")
+	err = admcl.AddSignatureToDefferedTx(id)
+	require.NoError(t, err)
+	log.Lvl1("[INFO] Admin 2 sign the transaction")
+	err = admcl2.AddSignatureToDefferedTx(id)
+	require.NoError(t, err)
+	log.Lvl1("[INFO] Admin 2 try to exec the transaction")
+	err = admcl.ExecDefferedTx(id)
+	require.NoError(t, err)
+	log.Lvl1("[INFO] Admin 2 try to exec the transaction")
+	_, err = lib.GetDarcByID(admcl.bcl, adminDarc.GetID())
+	require.NoError(t, err)
+	log.Lvl1("[INFO] Tx successfully executed, admin 3 has been added to the admin darc")
+}
