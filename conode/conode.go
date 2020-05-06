7712fbd0e9f948f2719d6f11ae86d972b759aab7
zahrafarsijani <zahrafarsijani@hotmail.com>
2020-05-06 22:18:38 +0200

    refactor code

--- conode/conode.go
+++ conode/conode.go
@@ -23,7 +23,7 @@ import (
 	"reflect"
 	"time"
 
-	_ "github.com/medchain/server"
+	_ "github.com/medchain/services"
 
 	"go.dedis.ch/cothority/v3"
 	status "go.dedis.ch/cothority/v3/status/service"
